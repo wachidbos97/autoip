146fed41f3fb17b460ece4f673e91e7742dfbc4a
wachidbos97 <wachidanwar61605@gmail.com>
2025-02-15 02:56:09 +0700

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 source "$BASE_DIR/config.sh"
-source "$BASE_DIR/helpers/adb_helper.sh"
+source "$BASE_DIR/logger.sh"
 
 FAIL_COUNT=0
 
 while true; do
     for DOMAIN in "${DOMAINS[@]}"; do
-        adb_ping $DOMAIN
+        ping -c 1 $DOMAIN > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             FAIL_COUNT=$((FAIL_COUNT + 1))
+            logger "Ping gagal ke $DOMAIN"
         else
             FAIL_COUNT=0
+            logger "Ping berhasil ke $DOMAIN"
         fi
 
         if [ $FAIL_COUNT -ge $PING_FAILURE_THRESHOLD ]; then
-            adb_toggle_data off
-            logger "Data dimatikan setelah $FAIL_COUNT kegagalan"
+            logger "Menonaktifkan data setelah $FAIL_COUNT kegagalan"
             sleep 10 # Tunggu sebelum mencoba lagi
-            adb_toggle_data on
             FAIL_COUNT=0
         fi
     done
