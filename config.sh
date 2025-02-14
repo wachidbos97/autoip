b9d715742fefd616e365cba80b9f5d026db5bd56
wachidbos97 <wachidanwar61605@gmail.com>
2025-02-15 02:58:41 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,4 @@
-# Daftar domain/IP untuk ping
-DOMAINS=("google.com" "8.8.8.8")
-
-# Jumlah kegagalan ping untuk mematikan data
+# Konfigurasi default
+DOMAINS=("google.com" "github.com")
+PING_INTERVAL=10
 PING_FAILURE_THRESHOLD=3
-
-# Interval ping dalam detik
-PING_INTERVAL=5
